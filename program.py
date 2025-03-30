6f295167bc80e6e66414652ca8edb49d9f929cf7
Nomoum <152312089+Nomoum@users.noreply.github.com>
2025-03-31 01:05:26 +0800

    Update program.py

--- program.py
+++ program.py
@@ -1,8 +1,9 @@
 import ast
 import requests
 
+url = "https://rt.data.gov.hk/v1/transport/mtr/getSchedule.php?line=TML&sta=HOM"
 # Sending an HTTP GET request to fetch data from the specified API URL
-web = requests.get("https://rt.data.gov.hk/v1/transport/mtr/getSchedule.php?line=TML&sta=HOM")
+web = requests.get(url)
 # Change the element scraped from the URL to dictionary
 dictionary = ast.literal_eval(web.text)
 
