5b398479dc4688bebc5a5a242bb21a96b8b2a638
Nomoum <152312089+Nomoum@users.noreply.github.com>
2025-04-17 00:49:00 +0800

    Add files via upload

---
+++ map_data_eng.py
@@ -0,0 +1,397 @@
+station_english_map = {
+    # East Rail Line
+    "Admiralty": "ADM",
+    "Exhibition Centre": "EXC",
+    "Hung Hom": "HUH",
+    "Mong Kok East": "MKK",
+    "Kowloon Tong": "KOT",
+    "Tai Wai": "TAW",
+    "Sha Tin": "SHT",
+    "Fo Tan": "FOT",
+    "Racecourse": "RAC",
+    "University": "UNI",
+    "Tai Po Market": "TAP",
+    "Tai Wo": "TWO",
+    "Fanling": "FAN",
+    "Sheung Shui": "SHS",
+    "Lo Wu": "LOW",
+    "Lok Ma Chau": "LMC",
+    # South Island Line
+    "Ocean Park": "OCP",
+    "Wong Chuk Hang": "WCH",
+    "Lei Tung": "LET",
+    "South Horizons": "SOH",
+    # Tuen Ma Line
+    "Wu Kai Sha": "WKS",
+    "Ma On Shan": "MOS",
+    "Heng On": "HEO",
+    "Tai Shui Hang": "TSH",
+    "Shek Mun": "SHM",
+    "City One": "CIO",
+    "Sha Tin Wai": "STW",
+    "Che Kung Temple": "CKT",
+    "Hin Keng": "HIK",
+    "Diamond Hill": "DIH",
+    "Kai Tak": "KAT",
+    "Sung Wong Toi": "SUW",
+    "To Kwa Wan": "TKW",
+    "Ho Man Tin": "HOM",
+    "East Tsim Sha Tsui": "ETS",
+    "Austin": "AUS",
+    "Nam Cheong": "NAC",
+    "Mei Foo": "MEF",
+    "Tsuen Wan West": "TWW",
+    "Kam Sheung Road": "KSR",
+    "Yuen Long": "YUL",
+    "Long Ping": "LOP",
+    "Tin Shui Wai": "TIS",
+    "Siu Hong": "SIH",
+    "Tuen Mun": "TUM",
+    # Kwun Tong Line
+    "Whampoa": "WHA",
+    "Yau Ma Tei": "YMT",
+    "Mong Kok": "MOK",
+    "Prince Edward": "PRE",
+    "Shek Kip Mei": "SKM",
+    "Lok Fu": "LOF",
+    "Wong Tai Sin": "WTS",
+    "Choi Hung": "CHH",
+    "Kowloon Bay": "KOB",
+    "Ngau Tau Kok": "NTK",
+    "Kwun Tong": "KWT",
+    "Lam Tin": "LAT",
+    "Yau Tong": "YAT",
+    "Tiu Keng Leng": "TIK",
+    # Tsuen Wan Line
+    "Central": "CEN",
+    "Tsim Sha Tsui": "TST",
+    "Jordan": "JOR",
+    "Sham Shui Po": "SSP",
+    "Cheung Sha Wan": "CSW",
+    "Lai Chi Kok": "LCK",
+    "Lai King": "LAK",
+    "Kwai Fong": "KWF",
+    "Kwai Hing": "KWH",
+    "Tai Wo Hau": "TWH",
+    "Tsuen Wan": "TSW",
+    # Island Line
+    "Kennedy Town": "KET",
+    "HKU": "HKU",
+    "Sai Ying Pun": "SYP",
+    "Sheung Wan": "SHW",
+    "Wan Chai": "WAC",
+    "Causeway Bay": "CAB",
+    "Tin Hau": "TIH",
+    "Fortress Hill": "FOH",
+    "North Point": "NOP",
+    "Quarry Bay": "QUB",
+    "Tai Koo": "TAK",
+    "Sai Wan Ho": "SWH",
+    "Shau Kei Wan": "SKW",
+    "Heng Fa Chuen": "HFC",
+    "Chai Wan": "CHW",
+    # Tseung Kwan O Line
+    "Tseung Kwan O": "TKO",
+    "Hang Hau": "HAH",
+    "Po Lam": "POA",
+    "LOHAS Park": "LHP",
+    # Tung Chung Line
+    "Hong Kong": "HOK",
+    "Kowloon": "KOW",
+    "Olympic": "OLY",
+    "Tsing Yi": "TSY",
+    "Sunny Bay": "SUN",
+    "Tung Chung": "TUC",
+    # Disneyland Resort Line
+    "Disneyland Resort": "DIS"
+}
+
+
+
+reverse_station_english_map = {EN: CH for CH, EN in station_english_map.items()}
+
+
+line_english_map = {
+    "EAL": "East Rail Line",
+    "SIL": "South Island Line",
+    "TML": "Tuen Ma Line",
+    "KTL": "Kwun Tong Line",
+    "TWL": "Tsuen Wan Line",
+    "ISL": "Island Line",
+    "TKL": "Tseung Kwan O Line",
+    "TCL": "Tung Chung Line",
+    "DRL": "Disneyland Resort Line",
+
+
+}
+
+
+
+map = {
+    # 港岛线
+    "KET": {"HKU": 2},  # 坚尼地城
+    "HKU": {"KET": 2, "SYP": 2},  # 香港大学
+    "SYP": {"HKU": 2, "SHW": 2},  # 西营盘
+    "SHW": {"SYP": 2, "CEN": 2},  # 上环
+    "CEN": {"SHW": 2, "ADM": 4, "HOK": 5},  # 中环
+    "ADM": {"CEN":2,"WAC": 2, "TST": 4, "EXC": 4, "OCP": 6},  # 金钟（荃湾线、东铁线、南港岛线换乘）
+    "WAC": {"ADM": 2, "CAB": 2},  # 湾仔
+    "CAB": {"WAC": 2, "TIH": 2},  # 铜锣湾
+    "TIH": {"CAB": 2, "FOH": 2},  # 天后
+    "FOH": {"TIH": 2, "NOP": 2},  # 炮台山
+    "NOP": {"FOH": 2, "QUB": 2},  # 北角（将军澳线换乘）
+    "QUB": {"NOP": 2, "TAK": 2, "YAT": 5},  # 鲗鱼涌（将军澳线换乘）
+    "TAK": {"QUB": 2, "SWH": 3},  # 太古
+    "SWH": {"TAK": 3, "SKW": 2},  # 西湾河
+    "SKW": {"SWH": 2, "HFC": 2},  # 筲箕湾
+    "HFC": {"SKW": 2, "CHW": 3},  # 杏花邨
+    "CHW": {"HFC": 3},  # 柴湾
+
+    # 荃湾线
+    "CEN": {"SHW": 2, "ADM": 2, "HOK": 5},  # 中环
+    "ADM": {"CEN":2,"WAC": 2, "TST": 4, "EXC": 4, "OCP": 6}, # 金钟
+    "TST": {"ADM": 4, "JOR": 2},  # 尖沙咀
+    "JOR": {"TST": 2, "YMT": 2},  # 佐敦
+    "YMT": {"JOR": 2, "MOK": 2, "HOM": 3},  # 油麻地（观塘线换乘）
+    "MOK": {"YMT": 2, "PRE": 1},  # 旺角（观塘线换乘）
+    "PRE": {"MOK": 1, "SSP": 2, "SKM": 2},  # 太子
+    "SSP": {"PRE": 2, "CSW": 2},  # 深水埗
+    "CSW": {"SSP": 2, "LCK": 2},  # 长沙湾
+    "LCK": {"CSW": 2, "MEF": 2},  # 荔枝角
+    "MEF": {"LCK": 2, "LAK": 3, "TWW": 7, "NAC": 4},  # 美孚（屯马线换乘）
+    "LAK": {"MEF": 3, "KWF": 2, "TSY": 2, "NAC": 3},  # 荔景（东涌线换乘）
+    "KWF": {"LAK": 2, "KWH": 2},  # 葵芳
+    "KWH": {"KWF": 2, "TWH": 2},  # 葵兴
+    "TWH": {"KWH": 2, "TSW": 2},  # 大窝口
+    "TSW": {"TWH": 2},  # 荃湾
+
+    # 屯马线
+    "WKS": {"MOS": 2},  # 乌溪沙
+    "MOS": {"WKS": 2, "HEO": 2},  # 马鞍山
+    "HEO": {"MOS": 2, "TSH": 2},  # 恒安
+    "TSH": {"HEO": 2, "SHM": 4},  # 大水坑
+    "SHM": {"TSH": 4, "CIO": 2},  # 石门
+    "CIO": {"SHM": 2, "STW": 2},  # 第一城
+    "STW": {"CIO": 2, "CKT": 2},  # 沙田围
+    "CKT": {"STW": 2, "TAW": 2},  # 车公庙
+    "TAW": {"CKT": 2, "HIK": 2, "SHT": 4, "KOT": 6},  # 大围（东铁线换乘）
+    "HIK": {"TAW": 2, "DIH": 6},  # 显径
+    "DIH": {"HIK": 6, "KAT": 2, "WTS": 2, "CHH": 2},  # 钻石山（观塘线换乘）
+    "KAT": {"DIH": 2, "SUW": 2},  # 启德
+    "SUW": {"KAT": 2, "TKW": 2},  # 宋皇台
+    "TKW": {"SUW": 2, "HOM": 2},  # 土瓜湾
+    "HOM": {"TKW": 2, "HUH": 2, "YMT": 3, "WHA": 2},  # 何文田（观塘线换乘）
+    "HUH": {"HOM": 2, "ETS": 3, "MKK": 3, "EXC": 5},  # 红磡（东铁线换乘）
+    "ETS": {"HUH": 3, "AUS": 3},  # 尖东
+    "AUS": {"ETS": 3, "NAC": 4},  # 柯士甸
+    "NAC": {"AUS": 4, "MEF": 4, "LAK": 3, "OLY": 1},  # 南昌（东涌线换乘）
+    "MEF": {"NAC": 4, "TWW": 7, "LAK": 3, "LCK": 2},  # 美孚
+    "TWW": {"MEF": 7, "KSR": 13},  # 荃湾西
+    "KSR": {"TWW": 13, "YUL": 5},  # 锦上路
+    "YUL": {"KSR": 5, "LOP": 2},  # 元朗
+    "LOP": {"YUL": 2, "TIS": 4},  # 朗屏
+    "TIS": {"LOP": 4, "SIH": 8},  # 天水围
+    "SIH": {"TIS": 8, "TUM": 3},  # 兆康
+    "TUM": {"SIH": 3},  # 屯门
+
+    # 东铁线
+    "ADM": {"CEN":2,"WAC": 2, "TST": 4, "EXC": 4, "OCP": 6}, # 金钟
+    "EXC": {"ADM": 4, "HUH": 5},  # 会展
+    "HUH": {"HOM": 2, "ETS": 3, "MKK": 3, "EXC": 5}, #红磡
+    "MKK": {"HUH": 3,"KOT": 3},  # 旺角东
+    "KOT": {"MKK": 3, "TAW": 6, "SKM": 2, "LOF": 2},  # 九龙塘（观塘线、荃湾线换乘）
+    "TAW": {"KOT": 6, "SHT": 4, "CKT": 2, "HIK": 2},  # 大围（屯马线换乘）
+    "SHT": {"TAW": 4, "FOT": 3},  # 沙田
+    "FOT": {"SHT": 3, "RAC": 20, "UNI": 3,},  # 火炭
+    "RAC": {"FOT": 20},  # 马场
+    "UNI": {"FOT": 3, "TAP": 7},  # 大学
+    "TAP": {"UNI": 7, "TWO": 2},  # 大埔墟
+    "TWO": {"TAP": 2, "FAN": 7},  # 太和
+    "FAN": {"TWO": 7, "SHS": 3},  # 粉岭
+    "SHS": {"FAN": 3, "LOW": 5, "LMC": 8},  # 上水
+    "LOW": {"SHS": 5},  # 罗湖
+    "LMC": {"SHS": 8},  # 落马洲
+
+    # 观塘线
+    "WHA": {"HOM": 2},  # 黄埔
+    "HOM": {"WHA": 2, "YMT": 3, "TKW": 2, "HUH": 2},  # 何文田（屯马线换乘）
+    "YMT": {"HOM": 3, "MOK": 2, "JOR": 2},  # 油麻地（荃湾线换乘）
+    "MOK": {"YMT": 2, "PRE": 1},  # 旺角（荃湾线换乘）
+    "PRE": {"MOK": 1, "SKM": 2, "SSP": 2},  # 太子
+    "SKM": {"PRE": 2, "KOT": 2},  # 石硤尾
+    "KOT": {"SKM": 2, "LOF": 2, "MKK": 3, "TAW": 6},  # 九龙塘（东铁线、荃湾线换乘）
+    "LOF": {"KOT": 2, "WTS": 2},  # 乐富
+    "WTS": {"LOF": 2, "DIH": 2},  # 黄大仙
+    "DIH": {"WTS": 2, "CHH": 2, "HIK": 6, "KAT": 2},  # 钻石山（屯马线换乘）
+    "CHH": {"DIH": 2, "KOB": 2},  # 彩虹
+    "KOB": {"CHH": 2, "NTK": 2},  # 九龙湾
+    "NTK": {"KOB": 2, "KWT": 2},  # 牛头角
+    "KWT": {"NTK": 2, "LAT": 2},  # 观塘
+    "LAT": {"KWT": 2, "YAT": 2},  # 蓝田
+    "YAT": {"LAT": 2, "TIK": 4, "QUB": 5},  # 油塘（将军澳线换乘）
+    "TIK": {"YAT": 4, "TKO": 2},  # 调景岭
+
+    # 南港岛线
+    "OCP": {"ADM": 6, "WCH": 2},  # 海洋公园
+    "WCH": {"OCP": 2, "LET": 2},  # 黄竹坑
+    "LET": {"WCH": 2, "SOH": 3},  # 利东
+    "SOH": {"LET": 3},  # 海怡半岛
+
+    # 将军澳线
+    "NOP": {"QUB": 2, "FOH": 2},  # 北角（港岛线换乘）
+    "QUB": {"NOP": 2, "YAT": 5, "TAK": 2},  # 鲗鱼涌（港岛线换乘）
+    "YAT": {"QUB": 5, "TIK": 4, "LAT": 2},  # 油塘（观塘线换乘）
+    "TIK": {"YAT": 4, "TKO": 2},  # 调景岭
+    "TKO": {"TIK": 2, "HAH": 2, "LHP": 6},  # 将军澳
+    "HAH": {"TKO": 2, "POA": 2},  # 坑口
+    "POA": {"HAH": 2},  # 宝琳
+    "LHP": {"TKO": 6},  # 康城
+
+    # 东涌线
+    "HOK": {"KOW": 2, "CEN": 5},  # 香港
+    "KOW": {"HOK": 2, "OLY": 2},  # 九龙
+    "OLY": {"KOW": 2, "NAC": 1},  # 奥运
+    "NAC": {"OLY": 1, "LAK": 3, "AUS": 4, "MEF": 4},  # 南昌（屯马线换乘）
+    "LAK": {"NAC": 3, "TSY": 2, "MEF": 3, "KWF": 2},  # 荔景（荃湾线换乘）
+    "TSY": {"LAK": 2, "SUN": 7},  # 青衣
+    "SUN": {"TSY": 7, "TUC": 8, "DIS": 7},  # 欣澳（迪士尼线换乘）
+    "TUC": {"SUN": 8},  # 东涌
+
+    # 迪士尼线
+    "SUN": {"DIS": 7, "TSY": 7, "TUC": 8},  # 欣澳（东涌线换乘）
+    "DIS": {"SUN": 7}  # 迪士尼
+
+
+
+}
+
+
+
+station_line = {
+    # 港岛线
+    "KET": ["ISL"],
+    "HKU": ["ISL"],
+    "SYP": ["ISL"],
+    "SHW": ["ISL"],
+    "CEN": ["ISL", "TWL"],
+    "ADM": ["ISL", "TWL", "EAL", "SIL"],
+    "WAC": ["ISL"],
+    "CAB": ["ISL"],
+    "TIH": ["ISL"],
+    "FOH": ["ISL"],
+    "NOP": ["ISL", "TKL"],
+    "QUB": ["ISL", "TKL"],
+    "TAK": ["ISL"],
+    "SWH": ["ISL"],
+    "SKW": ["ISL"],
+    "HFC": ["ISL"],
+    "CHW": ["ISL"],
+
+    # 荃湾线
+    "TST": ["TWL"],
+    "JOR": ["TWL"],
+    "YMT": ["TWL", "KTL"],
+    "MOK": ["TWL", "KTL"],
+    "PRE": ["TWL"],
+    "SSP": ["TWL"],
+    "CSW": ["TWL"],
+    "LCK": ["TWL"],
+    "MEF": ["TWL", "TML"],
+    "LAK": ["TWL", "TML", "TCL"],
+    "KWF": ["TWL"],
+    "KWH": ["TWL"],
+    "TWH": ["TWL"],
+    "TSW": ["TWL"],
+
+    # 屯马线
+    "WKS": ["TML"],
+    "MOS": ["TML"],
+    "HEO": ["TML"],
+    "TSH": ["TML"],
+    "SHM": ["TML"],
+    "CIO": ["TML"],
+    "STW": ["TML"],
+    "CKT": ["TML"],
+    "TAW": ["TML", "EAL"],
+    "HIK": ["TML"],
+    "DIH": ["TML", "KTL"],
+    "KAT": ["TML"],
+    "SUW": ["TML"],
+    "TKW": ["TML"],
+    "HOM": ["TML", "KTL"],
+    "HUH": ["TML", "EAL"],
+    "ETS": ["TML"],
+    "AUS": ["TML"],
+    "NAC": ["TML", "TCL"],
+    "TWW": ["TML"],
+    "KSR": ["TML"],
+    "YUL": ["TML"],
+    "LOP": ["TML"],
+    "TIS": ["TML"],
+    "SIH": ["TML"],
+    "TUM": ["TML"],
+
+    # 东铁线
+    "EXC": ["EAL"],
+    "MKK": ["EAL"],
+    "KOT": ["EAL", "KTL"],
+    "TAW": ["EAL", "TML"],
+    "SHT": ["EAL"],
+    "FOT": ["EAL"],
+    "RAC": ["EAL"],
+    "UNI": ["EAL"],
+    "TAP": ["EAL"],
+    "TWO": ["EAL"],
+    "FAN": ["EAL"],
+    "SHS": ["EAL"],
+    "LOW": ["EAL"],
+    "LMC": ["EAL"],
+
+    # 观塘线
+    "WHA": ["KTL"],
+    "HOM": ["KTL", "TML"],
+    "YMT": ["KTL", "TWL"],
+    "MOK": ["KTL", "TWL"],
+    "PRE": ["KTL", "TWL"],
+    "SKM": ["KTL"],
+    "KOT": ["KTL", "EAL"],
+    "LOF": ["KTL"],
+    "WTS": ["KTL"],
+    "DIH": ["KTL", "TML"],
+    "CHH": ["KTL"],
+    "KOB": ["KTL"],
+    "NTK": ["KTL"],
+    "KWT": ["KTL"],
+    "LAT": ["KTL"],
+    "YAT": ["KTL", "TKL"],
+    "TIK": ["KTL", "TKL"],
+
+    # 南港岛线
+    "OCP": ["SIL"],
+    "WCH": ["SIL"],
+    "LET": ["SIL"],
+    "SOH": ["SIL"],
+
+    # 将军澳线
+    "NOP": ["TKL"],
+    "QUB": ["TKL"],
+    "TKO": ["TKL"],
+    "HAH": ["TKL"],
+    "POA": ["TKL"],
+    "LHP": ["TKL"],
+
+    # 东涌线
+    "HOK": ["TCL"],
+    "KOW": ["TCL"],
+    "OLY": ["TCL"],
+    "NAC": ["TCL", "TML"],
+    "LAK": ["TCL", "TWL"],
+    "TSY": ["TCL"],
+    "SUN": ["TCL", "DRL"],
+    "TUC": ["TCL"],
+
+    # 迪士尼线
+    "SUN": ["DRL", "TCL"],
+    "DIS": ["DRL"]
+
+}
